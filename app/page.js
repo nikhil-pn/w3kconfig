6c38ad884eeccdc885a0fe871f147d893dc9b6ab
AaroneGeorge <aaroneofficial1879@gmail.com>
2024-08-09 19:18:40 +0530

    issues fixed in local

--- app/page.js
+++ app/page.js
@@ -1,28 +1,16 @@
-// app/page.js (or wherever your main component is)
-
 "use client";
-<<<<<<< HEAD
 
-=======
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
-
-import { AnimatePresence } from "framer-motion";
-import Preloader from "../components/Preloader";
->>>>>>> a268ec058a0cb550a7012011cc7d88536f8df632
 import Navbar from "../components/NavBar/Navbar";
 import Preloader from "../components/Preloader";
 import { AnimatePresence } from "framer-motion";
 import dynamic from "next/dynamic";
-<<<<<<< HEAD
-import { useEffect, useState } from "react";
-=======
 import Timer from "@/components/Timer/timer";
 import Word from "@/components/Event-description/Word";
 
 const paragraph =
   "Gm GM, listen up! This site’s got all the deets on the event—speakers, location, Bounty's, all that jazz. But, you gotta hunt for it, fam!  Happy Digging                                 First hint: All the secrets are buried deep in the console.";
->>>>>>> a268ec058a0cb550a7012011cc7d88536f8df632
 
 const Scene = dynamic(() => import("@/components/Scene"), {
   ssr: false,
@@ -59,19 +47,6 @@ export default function Home() {
   }
 
   return (
-<<<<<<< HEAD
-    <>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow flex items-center justify-center">
-          <AnimatePresence mode="wait">
-            {isLoading && <Preloader />}
-          </AnimatePresence>
-          <Scene />
-        </main>
-      </div>
-    </>
-=======
     <div className="flex flex-col min-h-screen bg-black pb-24">
       <Navbar />
       <main className="flex-grow items-center justify-center">
@@ -83,6 +58,5 @@ export default function Home() {
       <Word paragraph={paragraph} />
       <div className="mt-20"></div>
     </div>
->>>>>>> a268ec058a0cb550a7012011cc7d88536f8df632
   );
 }
